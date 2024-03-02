113978cae999acdee5bc1c4f913609d0341fdb3f
n3m0n10 <nemonetoifsc@gmail.com>
2024-03-02 11:23:26 -0300

    player added
    base do jogador

--- player.py
+++ player.py
@@ -3,11 +3,7 @@ import pygame
 
 class player():
 
-<<<<<<< HEAD
-    def __init__(self,screen, color, radius, posit_y):
-=======
     def __init__(self,screen, color, radius, posit_x):
->>>>>>> Player
         self.screen = screen
         self.color = color
         self.radius = radius
@@ -15,27 +11,16 @@ class player():
         self.player_vel = 0
 
 
-<<<<<<< HEAD
-        self.player_pos = pygame.Vector2(screen.get_width() / 2, posit_y)
-=======
         self.player_pos = pygame.Vector2(posit_x ,screen.get_height() / 2)
->>>>>>> Player
 
     def atualize(self,dt, tela:tuple):
         keys = pygame.key.get_pressed()
 
         #otimizar com o 'if key_down'
-<<<<<<< HEAD
-        #if keys[pygame.K_UP]:
-        #    self.player_pos.y -= 300 *dt
-        #if keys[pygame.K_DOWN]:
-        #    self.player_pos.y += 300 *dt
-=======
         if keys[pygame.K_UP]:
             self.player_pos.y -= 300 *dt
         if keys[pygame.K_DOWN]:
             self.player_pos.y += 300 *dt
->>>>>>> Player
         if keys[pygame.K_LEFT]:
             self.player_pos.x -= 300 *dt
         if keys[pygame.K_RIGHT]:
@@ -49,17 +34,10 @@ class player():
             self.player_pos.x = tela[0] - self.radius
 
 
-<<<<<<< HEAD
-        #if self.player_pos.y <= 0 + self.radius:
-        #    self.player_pos.y = 0 + self.radius
-        #if self.player_pos.y >= tela[1] - self.radius:
-        #    self.player_pos.y = tela[1] - self.radius
-=======
         if self.player_pos.y <= 0 + self.radius:
             self.player_pos.y = 0 + self.radius
         if self.player_pos.y >= tela[1] - self.radius:
             self.player_pos.y = tela[1] - self.radius
->>>>>>> Player
 
 
 
