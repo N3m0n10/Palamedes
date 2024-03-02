e72d436e9b33495c72f30e2f00d0e5ee133b6209
n3m0n10 <nemonetoifsc@gmail.com>
2024-03-01 22:49:53 -0300

    criando jogador

---
+++ player.py
@@ -0,0 +1,44 @@
+import pygame
+
+
+class player():
+
+    def __init__(self,screen, color, radius, posit_x):
+        self.screen = screen
+        self.color = color
+        self.radius = radius
+
+        self.player_vel = 0
+
+
+        self.player_pos = pygame.Vector2(posit_x ,screen.get_height() / 2)
+
+    def atualize(self,dt, tela:tuple):
+        keys = pygame.key.get_pressed()
+
+        #otimizar com o 'if key_down'
+        if keys[pygame.K_UP]:
+            self.player_pos.y -= 300 *dt
+        if keys[pygame.K_DOWN]:
+            self.player_pos.y += 300 *dt
+        if keys[pygame.K_LEFT]:
+            self.player_pos.x -= 300 *dt
+        if keys[pygame.K_RIGHT]:
+            self.player_pos.x += 300 *dt
+
+        
+   
+        if self.player_pos.x <= 0 + self.radius:
+            self.player_pos.x = 0 + self.radius
+        if self.player_pos.x >= tela[0] - self.radius:
+            self.player_pos.x = tela[0] - self.radius
+
+
+        if self.player_pos.y <= 0 + self.radius:
+            self.player_pos.y = 0 + self.radius
+        if self.player_pos.y >= tela[1] - self.radius:
+            self.player_pos.y = tela[1] - self.radius
+
+
+
+        pygame.draw.circle(self.screen,self.color, self.player_pos, self.radius)
---
+++ teste_nemo.py
@@ -0,0 +1,42 @@
+# Example file showing a circle moving on screen
+import pygame
+from ball import ball
+from player import player
+
+# pygame setup
+pygame.init()
+WIDTH, HEIGHT = 1280,720 #largura e altura
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+#fps
+clock = pygame.time.Clock()
+dt = 0
+
+ball1 = ball(screen,'red',40)
+player1 = player(screen, 'blue', 50, 180)
+player2 = player(screen, 'orange', 50, 1100)
+
+running = True
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("black")
+
+    #Bola
+    ball1.atualize(dt,(WIDTH, HEIGHT))
+    player1.atualize(dt,(WIDTH, HEIGHT))
+    player2.atualize(dt,(WIDTH, HEIGHT))
+   
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    # limits FPS to 60
+    # dt is delta time in seconds since last frame, used for framerate-
+    # independent physics.
+    dt = clock.tick(60) / 1000
+
+pygame.quit()
