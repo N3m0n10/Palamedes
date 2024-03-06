cc0bd3bbe36793c1e5ab8127ae187bc5c0d85f9f
n3m0n10 <nemonetoifsc@gmail.com>
2024-03-06 20:15:56 -0300

    color fix

--- placar.py
+++ placar.py
@@ -10,7 +10,7 @@ def pontuacao(ball_x_pos, screen_limit):
 
 def placar(screen, pontos_1 , pontos_2):
     text_placar_font = pygame.font.SysFont('tahoma', 100) 
-    text_placar1 = text_placar_font.render(f'{pontos_1}', True, (90, 100, 240))
-    text_placar2 = text_placar_font.render(f'{pontos_2}', True, (90, 100, 240))
+    text_placar1 = text_placar_font.render(f'{pontos_1}', True, ('White'))
+    text_placar2 = text_placar_font.render(f'{pontos_2}', True, ('White'))
     screen.blit(text_placar1, (380,100))
-    screen.blit(text_placar2, (980,100))
+    screen.blit(text_placar2, (900,100))
