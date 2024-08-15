504b985d87d5b56db100dd42b485063d2046bd5b
n3m0n10 <nemonetoifsc@gmail.com>
2024-08-14 21:29:02 -0300

    Menu apparence
    Apparence 1st state concluded, each game rect will be filled by an image.
    #to be done next: functional buttons,
      game redirections

---
+++ game_menu.py
@@ -0,0 +1,38 @@
+import pygame
+
+
+#create game list
+game_list = ["pong", "example", 'lorem ipsum', 'dolor sit', 'amet', 'consectetur', 'adipiscing', "secret"]   #add random mode
+num_games = len(game_list )-1 #desconsider the easter egg
+#set icons size and position
+icon_size = (300, 200)
+first_pos = (90, 100)
+#create pos list with first pos
+pos_list = []
+pos_list.append(first_pos)
+#create positions
+pointer = pos_list[0]
+for a in range(len(game_list) - 2):
+    next_pos = (pos_list[a][0] + 400 , pointer[1])
+    if next_pos[0] > 1280 - 300:    #When the next element goes off the screen, it starts on the next line
+        old_pointer = pointer
+        pointer = (90, old_pointer[1] + 250) #pointer fixed on first of each line (when the line is being coumposed)
+        next_pos = pointer
+        pos_list.append(next_pos)
+    else: pos_list.append(next_pos)    
+#create excell
+if pos_list[-1][1] > 720:
+    excell = pos_list[-1][1] + 1800
+else:
+    excell = 1800
+#not so secret easter egg position
+pos_list.append((790, excell - 250))
+
+#create screen size based on games number -> num will be used on main-> game_menu function
+surface_size = (1280, excell)
+
+#Define surface height  
+srfc_height = excell
+
+#create random mode <----------------
+print(pos_list)
--- superpongmain.py
+++ superpongmain.py
@@ -1,16 +1,23 @@
 import pygame 
 import colorsys
+from game_menu import excell, surface_size , game_list, icon_size, srfc_height, pos_list
+import subprocess
 
+#run game_menu.py as a subprocess
+subprocess.run(['python', 'game_menu.py'])
 ##PYGAME_SETUP---------------------------------------------
 pygame.init()
 pygame.font.init()
+#screen setup
 screen = pygame.display.set_mode((1280, 720))
+game_menu_surface = pygame.Surface((1280, srfc_height)) ###excell is calculated in game_menu.py
+#running and windw sets
 pygame.display.set_caption('PONG')
 clock = pygame.time.Clock()
 running = True
-icon_name = pygame.image.load('icon_name.jfif').convert()   #
+icon_name = pygame.image.load('icon_name.jfif').convert()   
 pygame.display.set_icon(icon_name)  #----> criar icône 
-##UNIVERSAIS_VAR-------------------------------------------------
+##UNIVERSAL_VAR-------------------------------------------------
 in_menu = True
 menu_font = pygame.font.SysFont('Comic Sans MS', 70)    #----->fazer função cria texto
 texto_selectplayer = menu_font.render('Press 0 for 1p or 1 for 2p', True, (90, 40, 240))
@@ -21,21 +28,28 @@ stages = iter(stages)
 estagio = next(stages)
 players = -1
 hue = 0 
-##FUNÇÕES_BASE
+run= 0
+#vars for game_menu  #translate to english
+# Variável de deslocamento para a rolagem
+scroll_offset = 0
+# Limite de rolagem (o máximo que a superfície pode rolar)
+scroll_limit = (srfc_height) - 720
+# Velocidade de rolagem
+scroll_speed = 20
+##BASE_FUNCTIONS---------------------------------------------
 def stage(estagio):
     match estagio:
         case 0 :
             return "menu"    #------->mudar nomes
         ##--------------->create select game ---> new vers == new gamemodes
         case 1 :
+            return "game_menu"
+
+        case 2 :
             return "select_player"
             
-        case 2 :
-            return "fase"
-
         case 3 :
-            #return pontuacao
-            pass
+            return "fase"
 
         case False :
             pass
@@ -56,6 +70,18 @@ def changeColor(hue):
         return (color[0]*255,color[1]*255,color[2]*255)
 
 
+##game_menu-----------------------------------------
+
+def game_menu_screen(game_list, pos_list,icon_size, excell, srfc_height,run):  #exclude unused vars
+    if run == 0:
+        for i, item in enumerate(game_list):
+            recta = pygame.Rect(pos_list[i], icon_size)
+            pygame.draw.rect(game_menu_surface, 'white' ,recta)
+    screen.blit(game_menu_surface, (0, -scroll_offset))
+##On click event must be created in #main #events
+#create  game icons --> clickable rectangle 
+    run = 1
+
 ##--------------------------------------------------------------------
 while running:
     # poll for events
@@ -72,9 +98,17 @@ while running:
                 players = 1
                 estagio = next(stages)
 
-        elif event.type == pygame.KEYDOWN and stage(estagio) == "menu":  #usar pygamr.key.get_pressed()
+        elif event.type == pygame.KEYDOWN and stage(estagio) == "menu":  #usar pygame.key.get_pressed()
             if event.key == pygame.K_SPACE:    
                 estagio = next(stages)
+
+        # Controle da rolagem usando as setas para cima e para baixo  ##not working ----> need to fix
+        elif event.type == pygame.MOUSEWHEEL:
+            if event.y < 0:  # Scroll para baixo
+                scroll_offset = min(scroll_offset + scroll_speed, scroll_limit)
+            elif event.y > 0:  # Scroll para cima
+                scroll_offset = max(scroll_offset - scroll_speed, 0)
+           
         
         
     # fill the screen with a color to wipe away anything from last frame
@@ -90,11 +124,14 @@ while running:
         cor = changeColor(hue)
         menu_screen(cor)
 
-    if stage(estagio) == "select_player": #error -------> repetindo 
+    if stage(estagio) == "game_menu": 
+        game_menu_screen(game_list, pos_list,icon_size, excell, srfc_height, run)
+
+    if stage(estagio) == "select_player": #error -------> repetindo #will berelocated to each game 
         select_player_screen()
 
-    if stage(estagio) == "fase":
-        with open("teste_nemo.py", "r") as file:  
+    if stage(estagio) == "fase": #will be renamed and triggered by game_menu
+        with open("teste_nemo.py", "r") as file:  #to be done: f'{game}.py
             exec(file.read(), {"__name__": ""})
 
     # flip() the display to put your work on screen
