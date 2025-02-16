fbd55bedd260da14502d87292ef87d2072a9abf2
n3m0n10 <nemonetoifsc@gmail.com>
2025-02-16 01:06:50 -0300

    working

--- solo_noble.py
+++ solo_noble.py
@@ -5,22 +5,61 @@ screen = pygame.display.set_mode((600, 600))
 
 initial_positions = []
 for i in range(7):
+    line = []
     for j in range(7):
         if i in [0,1,5,6] and j in [0,1,5,6]:
-            initial_positions.append([i, j, "blocked"])
+            line.append([ "blocked"])
         elif i == 3 and j == 3:
-            initial_positions.append([i, j, "empty"])
+            line.append([ "empty"])
         else:
-            initial_positions.append([i, j, "occupied"])
+            line.append([ "occupied"])
+    initial_positions.append(line)
+
+positions = initial_positions.copy()
 
 def move_piece(fst,scd):
-    pass
+    global positions
+    global selected_piece
+    global second_selected_piece
+    fst_x, fst_y = (fst[0]-44)//80, (fst[1]-44)//80
+    scd_x, scd_y = (scd[0]-44)//80 , (scd[1]-44)//80
+    print(fst_x, fst_y, scd_x, scd_y)
+    if positions[scd_x][scd_y] or positions[fst_x][fst_y] != ["empty"]:
+        print("eaaaaaaaaaaaaaaaaaaa")
+        if (fst_x == scd_x) and (fst_y == scd_y + 1):
+            if scd_y  > 0:
+                print("abbbbbbbbbbbb")
+                if positions[scd_x][scd_y-1] == ["empty"]:
+                    positions[fst_x][fst_y] = ["empty"]
+                    positions[scd_x][scd_y] = ["empty"]
+                    positions[scd_x][scd_y-1] = ["occupied"]
+                    print("a")
+        elif (fst_x == scd_x) and (fst_y == scd_y - 1):
+            if scd_y  < 6:
+                if positions[scd_x][scd_y+1] == ["empty"]:
+                    positions[fst_x][fst_y] = ["empty"]
+                    positions[scd_x][scd_y] = ["empty"]
+                    positions[scd_x][scd_y+1] = ["occupied"]
+        elif (fst_x == scd_x + 1) and (fst_y == scd_y):
+            if scd_x < 6:
+                if positions[scd_x-1][scd_y] == ["empty"]:
+                    positions[fst_x][fst_y] = ["empty"]
+                    positions[scd_x][scd_y] = ["empty"]
+                    positions[scd_x-1][scd_y] = ["occupied"]
+        elif (fst_x == scd_x - 1) and (fst_y == scd_y):
+            if scd_x > 0:
+                if positions[scd_x+1][scd_y] == ["empty"]:
+                    positions[fst_x][fst_y] = ["empty"]
+                    positions[scd_x][scd_y] = ["empty"]
+                    positions[scd_x+1][scd_y] = ["occupied"]
+    selected_piece = None
+    second_selected_piece = None
 
 clock = pygame.time.Clock()
 running = True
 selected_piece = None
 second_selected_piece = None
-
+button = pygame.rect.Rect(0,0,50,50)
     
 rects = [] #for mouse hover
 while running:
@@ -28,16 +67,17 @@ while running:
     screen.fill((30,30,30))
     pygame.draw.circle(screen, (0, 50, 190), (300, 300), 290)
 
-    for k in initial_positions:
-        match k[2]:
-            case "blocked":
-                pass
-            case "empty":
-                a = pygame.draw.circle(screen, (0, 0, 0), (k[0] * 80 + 60, k[1] * 80 + 60), 15)
-                rects.append(a)
-            case "occupied":
-                b = pygame.draw.circle(screen, "brown", (k[0] * 80 + 60, k[1] * 80 + 60), 16)
-                rects.append(b)
+    for j,line in enumerate(positions):
+        for k, piece in enumerate(line): 
+            match piece:
+                case ["blocked"]:
+                    pass
+                case ["empty"]:
+                    a = pygame.draw.circle(screen, (0, 0, 0), (j * 80 + 60, k * 80 + 60), 15)
+                    rects.append(a)
+                case ["occupied"]:
+                    b = pygame.draw.circle(screen, "brown", (j * 80 + 60, k * 80 + 60), 16)
+                    rects.append(b)
     
     for rect in rects:
         if rect.collidepoint(pygame.mouse.get_pos()):
@@ -60,17 +100,23 @@ while running:
                         print("second selected")
                         move_piece(selected_piece, second_selected_piece)
                         print(selected_piece,second_selected_piece)
+            if button.collidepoint(pygame.mouse.get_pos()):
+                print("reset")
+                second_selected_piece = None
+                selected_piece = None
+                positions = initial_positions.copy() ###TODO
+                print(positions)
 
 ##################################################################events
 
     
-    
+    pygame.draw.circle(screen, "orange", button.center, 15)
     if selected_piece is not None:
         pygame.draw.circle(screen, "green", selected_piece.center, 17,1)
     if second_selected_piece is not None:
         pygame.draw.circle(screen, "green", second_selected_piece.center, 17,1)
     
-                
+    #for a in initial_positions:
 
 
     pygame.display.flip()
